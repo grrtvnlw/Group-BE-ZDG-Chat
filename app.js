aa1e65afbce3c041d812c4435be39a9633da4fc6
David Foster <dlfosterii@yahoo.com>
2020-05-28 15:23:35 -0400

    app.js created

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening. Open http://localhost:${PORT} to view.`);
+});
