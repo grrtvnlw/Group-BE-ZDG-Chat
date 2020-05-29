98569595ac88f69585ac2b00051e6b34b8c07ac3
Zahria Shannon <zahria.shannon08@gmail.com>
2020-05-29 15:51:18 -0400

    adds routes and registration hash

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./models');
+const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -17,21 +19,44 @@ app.get('/', (req, res) => {
     })
 })
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-    res.render('livingroom', {
-        title: 'Welcome to ZDG Chat',
-    })
-});
-=======
 app.get('/mainroom', (req, res) => {
     res.render('mainchat', {
         title: 'ZDG Chat Main Room',
     })
 })
 
+app.post('/signup', (req, res) => {
+    const { username, email, password } = req.body
+    bcrypt.hash(password, 10, (err, hash) => {
+        db.User.create({
+            username: username,
+            email: email, 
+            password: hash,
+        }).then((result) => {
+            res.redirect('/');
+        });
+    });
+});
 
->>>>>>> 234c21dd1ace7e42c0ad71fe3eaf7260d5843d1a
+app.post('/signin', (req, res) => {
+    const { username, password } = req.body;
+    db.User.findOne( {where: { username: username } })
+        .then(User => {
+            bcrypt.compare(password, User.password, (err, match) => {
+                if (match) {
+                    // req.session.user = User;
+                    res.redirect('/mainroom');
+                }
+                else {
+                    res.send('Incorrect Password')
+                }
+            })
+        })
+        .catch(() => {
+            res.send('Username not found');
+        })
+
+})
 
 
 
